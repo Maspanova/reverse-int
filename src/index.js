1e6280da891ec4f2aea11f0b9a57a3c15beefcf1
Maspanova <105561456+Maspanova@users.noreply.github.com>
2023-02-09 14:20:15 +0300

    add funtion

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
+        let myFunc = num => Number(num);
+      
+    var intArr = Array.from(String(n), myFunc);
+      
+  
+ 
+return +(intArr.reverse()).join('')
   
 }
